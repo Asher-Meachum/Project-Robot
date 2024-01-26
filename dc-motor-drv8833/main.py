7f5d6a77f1fbda6a7be080b4570843901433334f
Asher Meachum <132758546+Asher-Meachum@users.noreply.github.com>
2024-01-26 10:27:12 -0600

    Update main.py
    Changes the Micropython utime library to the standard Python time library.

--- dc-motor-drv8833/main.py
+++ dc-motor-drv8833/main.py
@@ -1,6 +1,6 @@
 from robot_car import RobotCar
 from machine import Pin
-import utime
+import time
 
 # Pico W GPIO Pin
 LEFT_MOTOR_PIN_1 = 16
@@ -23,41 +23,41 @@ if __name__ == '__main__':
         for i in range(2):
             print("Moving forward")
             robot_car.move_forward()
-            utime.sleep(2)
+            time.sleep(2)
             print("Moving backward")
             robot_car.move_backward()
-            utime.sleep(2)
+            time.sleep(2)
             print("stop")
             robot_car.stop()
-            utime.sleep(2)
+            time.sleep(2)
             print("turn left")
             robot_car.turn_left()
-            utime.sleep(2)
+            time.sleep(2)
             print("turn right")
             robot_car.turn_right()
-            utime.sleep(2)
+            time.sleep(2)
             
         print("*********Testing speed*********")
         for i in range(2):
             print("Moving at 100% speed")
             robot_car.change_speed(100);
             robot_car.move_forward()
-            utime.sleep(2)
+            time.sleep(2)
             
             print("Moving at 50% speed")
             robot_car.change_speed(50);
             robot_car.move_forward()
-            utime.sleep(2)
+            time.sleep(2)
             
             print("Moving at 20% of speed")
             robot_car.change_speed(20);
             robot_car.move_forward()
-            utime.sleep(2)
+            time.sleep(2)
             
             print("Moving at 0% of speed or the slowest")
             robot_car.change_speed(0);
             robot_car.move_forward()
-            utime.sleep(2)
+            time.sleep(2)
             
         robot_car.deinit()
 
