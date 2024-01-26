712ff7f02d8e12a7c5c6baccaa97f350172108e1
Asher Meachum <132758546+Asher-Meachum@users.noreply.github.com>
2024-01-26 10:32:39 -0600

    Update main.py
    Use the SLEEP and LED pins.

--- dc-motor-drv8833/main.py
+++ dc-motor-drv8833/main.py
@@ -12,6 +12,9 @@ RIGHT_MOTOR_PIN_2 = 19
 SLEEP = Pin(15, Pin.OUT) # This allows the DRV8833 to be turned on.
 LED = Pin("LED", Pin.OUT) # When not connected to a computer or display, we can use this to know when the code starts, when it ends, and at what point we are in the code.
 
+SLEEP.value(1) # Turns pin 15 to high.
+LED.value(1) # Turns the onboard LED on.
+
 motor_pins = [LEFT_MOTOR_PIN_1, LEFT_MOTOR_PIN_2, RIGHT_MOTOR_PIN_1, RIGHT_MOTOR_PIN_2]
 
 # Create an instance of our robot car
@@ -59,8 +62,12 @@ if __name__ == '__main__':
             robot_car.change_speed(0);
             robot_car.move_forward()
             time.sleep(2)
-            
+
+        SLEEP.value(0)
+        LED.value(0)
         robot_car.deinit()
 
     except KeyboardInterrupt:
+        SLEEP.value(0)
+        LED.value(0)
         robot_car.deinit()
