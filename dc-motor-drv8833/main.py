be05f05f26583530431e62bf5f63b84969446a77
Asher Meachum <132758546+Asher-Meachum@users.noreply.github.com>
2024-02-07 09:55:16 -0600

    Organize main.py
    Comments were organized better and the name robot_car was shortened to car.

--- dc-motor-drv8833/main.py
+++ dc-motor-drv8833/main.py
@@ -1,6 +1,15 @@
+'''
+Forward - car.forward
+Backward - car.back
+Left - car.left
+Right - car.right
+Full speed - car.change_speed(100)
+Half speed - car.change_speed(50)
+No speed - car.change_speed(0)
+'''
 from robot_car import RobotCar
 from machine import Pin
-import time
+from time import sleep
 
 # Pico W GPIO Pin
 LEFT_MOTOR_PIN_1 = 16
@@ -18,29 +27,11 @@ LED.value(1) # Turns the onboard LED on.
 motor_pins = [LEFT_MOTOR_PIN_1, LEFT_MOTOR_PIN_2, RIGHT_MOTOR_PIN_1, RIGHT_MOTOR_PIN_2]
 
 # Create an instance of our robot car
-robot_car = RobotCar(motor_pins, 20000)
+car = RobotCar(motor_pins, 20000)
 
 if __name__ == '__main__':
     try:
-        '''
-        robot_car.forward()
-        robot_car.back()
-        robot_car.stop()
-        robot_car.left()
-        robot_car.right()
         
-        robot_car.change_speed(100); # Changes speed to 100%
-        robot_car.forward()
-            
-        robot_car.change_speed(50); # Changes speed to 50%
-        robot_car.forward()
-            
-        robot_car.change_speed(20); # Changes speed to 20%
-        robot_car.forward()
-            
-        robot_car.change_speed(0); #Changes speed to 0%
-        robot_car.forward()     
-        '''
         SLEEP.value(0)
         LED.value(0)
         robot_car.deinit()
