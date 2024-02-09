7295fae6ed6b67acfe7a748bc461aee76a8d5632
Asher Meachum <132758546+Asher-Meachum@users.noreply.github.com>
2024-02-09 13:54:52 -0600

    Abstraction in main.py

--- dc-motor-drv8833/main.py
+++ dc-motor-drv8833/main.py
@@ -1,15 +1,9 @@
-'''
-Forward - car.forward
-Backward - car.back
-Left - car.left
-Right - car.right
-Full speed - car.change_speed(100)
-Half speed - car.change_speed(50)
-No speed - car.change_speed(0)
-'''
 from robot_car import RobotCar
 from machine import Pin
 from time import sleep
+
+straight_time = 1.15
+turn_time = 0.38
 
 # Pico W GPIO Pin
 LEFT_MOTOR_PIN_1 = 16
@@ -21,13 +15,28 @@ RIGHT_MOTOR_PIN_2 = 19
 SLEEP = Pin(15, Pin.OUT) # This allows the DRV8833 to be turned on.
 LED = Pin("LED", Pin.OUT) # When not connected to a computer or display, we can use this to know when the code starts, when it ends, and at what point we are in the code.
 
-SLEEP.value(1) # Turns pin 15 to high.
-LED.value(1) # Turns the onboard LED on.
-
 motor_pins = [LEFT_MOTOR_PIN_1, LEFT_MOTOR_PIN_2, RIGHT_MOTOR_PIN_1, RIGHT_MOTOR_PIN_2]
 
-# Create an instance of our robot car
-car = RobotCar(motor_pins, 20000)
+SLEEP.value(1) # Turns pin 15 to high.
+LED.value(1) # Turns the onboard LED on.
+car = RobotCar(motor_pins, 20000) # Create an instance of the robot car
+
+def north():
+    car.forward()
+    sleep(straight_time)
+
+def south():
+    car.back()
+    sleep(straight_time)
+
+def west():
+    car.left()
+    sleep(turn_time)
+
+def east():
+    car.right():
+    sleep(turn_time)
+
 
 if __name__ == '__main__':
     try:
