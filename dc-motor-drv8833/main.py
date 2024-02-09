99c45b49d98d2e31f6de3ee1d073d651d0a43637
Asher Meachum <132758546+Asher-Meachum@users.noreply.github.com>
2024-02-09 14:48:16 -0600

    Increase readability in main.py

--- dc-motor-drv8833/main.py
+++ dc-motor-drv8833/main.py
@@ -7,22 +7,27 @@ Full speed - car.change_speed(100)
 Half speed - car.change_speed(50)
 No speed - car.change_speed(0)
 '''
+
 from robot_car import RobotCar
 from machine import Pin
 from time import sleep
+
 # Pico W GPIO Pin
 LEFT_MOTOR_PIN_1 = 16
 LEFT_MOTOR_PIN_2 = 17
 RIGHT_MOTOR_PIN_1 = 18
 RIGHT_MOTOR_PIN_2 = 19
+
 # Other GPIO Pins
 SLEEP = Pin(15, Pin.OUT) # This allows the DRV8833 to be turned on.
 LED = Pin("LED", Pin.OUT) # When not connected to a computer or display, we can use this to know when the code starts, when it ends, and at what point we are in the code.
+
+motor_pins = [LEFT_MOTOR_PIN_1, LEFT_MOTOR_PIN_2, RIGHT_MOTOR_PIN_1, RIGHT_MOTOR_PIN_2]
+
 SLEEP.value(1) # Turns pin 15 to high.
 LED.value(1) # Turns the onboard LED on.
-motor_pins = [LEFT_MOTOR_PIN_1, LEFT_MOTOR_PIN_2, RIGHT_MOTOR_PIN_1, RIGHT_MOTOR_PIN_2]
-# Create an instance of our robot car
-car = RobotCar(motor_pins, 20000)
+car = RobotCar(motor_pins, 20000) # Create an instance of our robot car
+
 if __name__ == '__main__':
     try:
 
