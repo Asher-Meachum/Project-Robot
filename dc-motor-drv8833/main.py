4d24e41d3f4977cc10c21de5b3820b8d43a33b85
Asher Meachum <132758546+Asher-Meachum@users.noreply.github.com>
2024-02-15 18:20:43 -0600

    Clean up main.py

--- dc-motor-drv8833/main.py
+++ dc-motor-drv8833/main.py
@@ -1,8 +1,4 @@
 '''
-Forward - car.forward
-Backward - car.back
-Left - car.left
-Right - car.right
 Full speed - car.change_speed(100)
 Half speed - car.change_speed(50)
 No speed - car.change_speed(0)
@@ -13,31 +9,26 @@ from machine import Pin
 from time import sleep
 from rp2 import bootsel_button
 
-# Pico W GPIO Pin
-LEFT_MOTOR_PIN_1 = 16
-LEFT_MOTOR_PIN_2 = 17
-RIGHT_MOTOR_PIN_1 = 18
-RIGHT_MOTOR_PIN_2 = 19
-
 # Other GPIO Pins
 SLEEP = Pin(15, Pin.OUT) # This allows the DRV8833 to be turned on.
 LED = Pin("LED", Pin.OUT) # When not connected to a computer or display, we can use this to know when the code starts, when it ends, and at what point we are in the code.
 
-motor_pins = [LEFT_MOTOR_PIN_1, LEFT_MOTOR_PIN_2, RIGHT_MOTOR_PIN_1, RIGHT_MOTOR_PIN_2]
+def initilization():
+    motor_pins = [16, 17, 18, 19]
+    SLEEP.value(1) # Turns pin 15 to high.
+    LED.value(1) # Turns the onboard LED on.
+    car = RobotCar(motor_pins, 20000) # Create an instance of our robot car
 
-SLEEP.value(1) # Turns pin 15 to high.
-LED.value(1) # Turns the onboard LED on.
-car = RobotCar(motor_pins, 20000) # Create an instance of our robot car
+def deinitialization():
+    SLEEP.value(0)
+    LED.value(0)
+    car.deinit()
 
-if __name__ == '__main__':
-    try:
-        if bootsel_button():
+try:
+    initialization()
+    if bootsel_button():
 
-            SLEEP.value(0)
-            LED.value(0)
-            car.deinit()
+        deinitialization()
 
-    except KeyboardInterrupt:
-        SLEEP.value(0)
-        LED.value(0)
-        car.deinit()
+except KeyboardInterrupt:
+    deinitialization()
