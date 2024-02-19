c4d0729d0bd8a0d2a77b3a1179e19129aab9b1fa
Asher Meachum <132758546+Asher-Meachum@users.noreply.github.com>
2024-02-19 11:51:09 -0600

    Update main.py for the new module

--- dc-motor-drv8833/main.py
+++ dc-motor-drv8833/main.py
@@ -1,49 +1,21 @@
-'''
-Full speed - car.change_speed(100)
-Half speed - car.change_speed(50)
-No speed - car.change_speed(0)
-'''
-
-from robot_car import RobotCar
+import backend as lib
 from machine import Pin
 from time import sleep
 from rp2 import bootsel_button
 
-# Other GPIO Pins
-SLEEP = Pin(15, Pin.OUT) # This allows the DRV8833 to be turned on.
 LED = Pin("LED", Pin.OUT) # When not connected to a computer or display, we can use this to know when the code starts, when it ends, and at what point we are in the code.
-
-motor_pins = [16, 17, 18, 19]
-SLEEP.value(1) # Turns pin 15 to high.
 LED.value(1) # Turns the onboard LED on.
-car = RobotCar(motor_pins, 20000) # Create an instance of our robot car
 
 def deinit():
-    SLEEP.value(0)
     LED.value(0)
-    car.deinit()
-
-def n():
-    car.forward()
-    sleep(1.15)
-
-def s():
-    car.back()
-    sleep(1.15)
-
-def w():
-    car.left()
-    sleep(0.38)
-
-def e():
-    car.right()
-    sleep(0.38)
+    lib.deinit()
 
 try:
     while True:
         if bootsel_button():
 
             deinit()
+            break
 
 except KeyboardInterrupt:
     deinit()
