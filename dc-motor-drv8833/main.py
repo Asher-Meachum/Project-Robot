23bcc2ebf487912f0baab2567f04f976e0f16c7e
Asher Meachum <132758546+Asher-Meachum@users.noreply.github.com>
2024-02-27 10:59:14 -0600

    Add garbage collection to main.py

--- dc-motor-drv8833/main.py
+++ dc-motor-drv8833/main.py
@@ -2,6 +2,9 @@ import backend as lib
 from machine import Pin
 from time import sleep
 from rp2 import bootsel_button
+import gc
+
+gc.disable()
 
 LED = Pin("LED", Pin.OUT) # When not connected to a computer or display, we can use this to know when the code starts, when it ends, and at what point we are in the code.
 LED.value(1) # Turns the onboard LED on.
@@ -9,10 +12,12 @@ LED.value(1) # Turns the onboard LED on.
 def deinit():
     LED.value(0)
     lib.deinit()
+    gc.collect()
 
 try:
     while True:
         if bootsel_button():
+            gc.collect()
 
             deinit()
             break
